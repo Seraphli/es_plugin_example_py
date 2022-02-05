a6a0aafb3afdf9a7d6c3720912acb7455e8b2528
SErAphLi <seraphlivery@gmail.com>
2022-02-06 00:45:22 +0800

    add example for config

--- plugin.py
+++ plugin.py
@@ -7,12 +7,15 @@ import uuid
 
 APP_NAME = "electron-spirit"
 PLUGIN_NAME = "ES Plugin Example"
+SHORT_NAME = "example"
+PLUGIN_SETTING = "plugin.setting.json"
 
 
 class PluginApi(socketio.AsyncClientNamespace):
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
         self.elem_count = 0
+        self.parent = parent
 
     def on_connect(self):
         print("Connected")
@@ -57,6 +60,7 @@ class PluginApi(socketio.AsyncClientNamespace):
 
     def on_update_bound(self, key, _type, bound):
         print("Update bound:", key, _type, bound)
+        self.parent.update_bound(key, _type, bound)
 
     def on_process_content(self, content):
         print("Process content:", content)
@@ -68,19 +72,43 @@ class PluginApi(socketio.AsyncClientNamespace):
 class Plugin(object):
     def __init__(self) -> None:
         self.load_config()
-        self.api = PluginApi()
+        self.api = PluginApi(self)
 
     def load_config(self):
         path = user_data_dir(APP_NAME, False, roaming=True)
         with codecs.open(path + "/api.json") as f:
             self.config = json.load(f)
         self.port = self.config["apiPort"]
+        try:
+            with codecs.open(PLUGIN_SETTING) as f:
+                self.cfg = json.load(f)
+        except:
+            self.cfg = {
+                "input_hook": "g",
+                "css": ".car { position: relative; width: 100%; height: 100%; padding: 10px; background-color: rgba(250, 250, 250, 200); border: 1px solid black; text-align: center; box-sizing: border-box; overflow: auto; }",
+                "basic": "<div class='car'>Hello</div>",
+                "basic_bound": {"x": 200, "y": 200, "w": 100, "h": 50},
+                "view": "https://www.baidu.com",
+                "view_bound": {"x": 300, "y": 300, "w": 300, "h": 300},
+            }
+        self.save_cfg()
+
+    def save_cfg(self):
+        with codecs.open(PLUGIN_SETTING, "w") as f:
+            json.dump(self.cfg, f)
+    
+    def update_bound(self, key, _type, bound):
+        if key == "basic-1":
+            self.cfg["basic_bound"] = bound
+        elif key == "view-1":
+            self.cfg["view_bound"] = bound
+        self.save_cfg()
 
     async def register(self):
         # Create a context for registering plugins
         # You can either use sample password or use complex password
         # You can also register multiple topic
-        ctx = {"topic": "foo", "pwd": str(uuid.uuid4())}
+        ctx = {"topic": SHORT_NAME, "pwd": str(uuid.uuid4())}
         await sio.emit("register_topic", ctx)
         self.ctx = ctx
         await sio.emit(
@@ -93,20 +121,20 @@ class Plugin(object):
 
     async def test_case(self):
         # get input 'foo' from like '^g foo'
-        await sio.emit("input_hook", data=(self.ctx, "g"))
-        css = ".car { position: relative; width: 100%; height: 100%; padding: 10px; background-color: rgba(250, 250, 250, 200); border: 1px solid black; text-align: center; box-sizing: border-box; overflow: auto; }"
+        await sio.emit("input_hook", data=(self.ctx, self.cfg["input_hook"]))
+        css = self.cfg["css"]
         await sio.emit("insert_css", data=(self.ctx, css))
         basic_elem = {
             "key": "basic-1",
             "type": 0,
-            "bound": {"x": 200, "y": 200, "w": 100, "h": 50},
-            "content": "<div class='car'>Hello</div>",
+            "bound": self.cfg["basic_bound"],
+            "content": self.cfg["basic"],
         }
         view_elem = {
             "key": "view-1",
             "type": 1,
-            "bound": {"x": 300, "y": 300, "w": 300, "h": 300},
-            "content": "https://www.baidu.com",
+            "bound": self.cfg["view_bound"],
+            "content": self.cfg["view"],
         }
         await sio.emit(
             "update_elem",
